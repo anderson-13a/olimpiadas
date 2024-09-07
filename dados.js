d2697c7101f5ea7de3ac53e814cd81da1fa40efb
Andesron Barros <109999081+anderson-13a@users.noreply.github.com>
2024-09-07 13:35:13 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela se tornou um ícone do esporte e inspira milhões de jovens.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro na categoria +78kg. Sua técnica e força a tornaram uma das principais atletas do judô mundial.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoka"
+
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que fez história nos Jogos Olímpicos de Tóquio 2020, conquistando a medalha de ouro no salto e a de prata no individual geral. Seu carisma e superação de lesões a tornaram um símbolo do esporte brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+
+    }
+];
+
